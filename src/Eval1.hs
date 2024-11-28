dc07d08d960a5d2d3b47e2d765858eb52166bb3b
Octavio <octaviorassi@gmail.com>
2024-11-28 15:29:46 -0300

    implemented simple state monad-based eval

--- src/Eval1.hs
+++ src/Eval1.hs
@@ -56,10 +56,66 @@ stepCommStar c    = stepComm c >>= \c' -> stepCommStar c'
 
 -- Evalua un paso de un comando
 stepComm :: MonadState m => Comm -> m Comm
-stepComm = undefined
+stepComm Skip                   = return Skip
+stepComm (Let v e)              = do  ev <- evalExp e
+                                      update v ev
+                                      return Skip
+stepComm (Seq Skip c2)          = return c2
+stepComm (Seq c1 c2)            = do  c1' <- stepComm c1
+                                      return (Seq c1' c2)
+stepComm (IfThenElse b c1 c2)   = do  bv <- evalExp b
+                                      if bv then return c1
+                                            else return c2
+stepComm r@(Repeat b c)         = return $ Seq c (IfThenElse b r Skip)
 
 -- Evalua una expresion
 evalExp :: MonadState m => Exp a -> m a
-evalExp = undefined
+evalExp (Const      n)  = return  n
+evalExp (Var        v)  = lookfor v
+evalExp (UMinus     n)  = evalUnary  negate n
+evalExp (Plus   e0 e1)  = evalBin (+)   e0 e1
+evalExp (Minus  e0 e1)  = evalBin (-)   e0 e1
+evalExp (Times  e0 e1)  = evalBin (*)   e0 e1
+evalExp (Div    e0 e1)  = evalBin div   e0 e1
 
+evalExp BTrue           = return True
+evalExp BFalse          = return False 
 
+evalExp (Not        e)  = evalUnary     not e
+evalExp (Lt     e0 e1)  = evalBin (<)   e0 e1
+evalExp (Gt     e0 e1)  = evalBin (>)   e0 e1
+evalExp (And    e0 e1)  = evalBin (&&)  e0 e1
+evalExp (Or     e0 e1)  = evalBin (&&)  e0 e1
+evalExp (Eq     e0 e1)  = evalBin (==)  e0 e1
+evalExp (NEq    e0 e1)  = evalBin (/=)  e0 e1
+
+evalExp (VarInc     s)  = varUpdate s succ
+evalExp (VarDec     s)  = varUpdate s pred
+
+evalBin :: MonadState m => (a -> b -> c) -> Exp a -> Exp b -> m c
+evalBin op e0 e1 = do v0 <- evalExp e0
+                      v1 <- evalExp e1
+                      return $ op v0 v1
+
+evalUnary :: MonadState m => (a -> b) -> Exp a -> m b
+evalUnary op e = do v <- evalExp e 
+                    return $ op v
+
+varUpdate :: MonadState m => String -> (a -> b) -> m b
+varUpdate s f = do  v <- lookfor s
+                    update s (f v)
+                    return $ f v
+
+{-
+
+  Lt ::Exp Int -> Exp Int -> Exp Bool
+  Gt ::Exp Int -> Exp Int -> Exp Bool
+  And ::Exp Bool -> Exp Bool -> Exp Bool
+  Or ::Exp Bool -> Exp Bool -> Exp Bool
+  Not ::Exp Bool -> Exp Bool
+  Eq ::Exp Int -> Exp Int -> Exp Bool
+  NEq ::Exp Int -> Exp Int -> Exp Bool
+  VarInc :: Variable -> Exp Int
+  VarDec :: Variable -> Exp Int
+
+-}
